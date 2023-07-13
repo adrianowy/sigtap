d691f15f1a655c4f119983cd2a126a17891418d9
Adriano Wanderley <adrianoabreu.wy@gmail.com>
2023-07-13 11:55:46 +0100

    Refactoring code

--- parsefiles/index.js
+++ parsefiles/index.js
@@ -1,52 +1,60 @@
-var fs = require("fs");
+const fs = require("fs");
+const path = require("path");
+const { promisify } = require("util");
 
-// add here the competence folder name
-const competencia = "202307"
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
 
-// add here the list of files to be converter
-const datasetList = ['tb_procedimento', 'tb_cid', 'rl_procedimento_cid'];
+// Add here the competence folder name
+const competencia = "202307";
 
-try {
-  for (const dataset of datasetList) {
-      const data = fs.readFileSync(
-        `./../competencia/${competencia}/${dataset}.txt`,
+// Add here the list of files to be converted
+const datasetList = ["tb_procedimento", "tb_cid", "rl_procedimento_cid"];
+
+async function convertFiles() {
+
+  try {
+    for (const dataset of datasetList) {
+
+      const data = await readFile(
+        path.join( __dirname, "..", "competencia", competencia, `${dataset}.txt` ),
         "latin1"
       );
 
-      const layout = fs.readFileSync(
-        `./../competencia/${competencia}/${dataset}_layout.txt`,
+      const layout = await readFile(
+        path.join( __dirname, "..", "competencia", competencia, `${dataset}_layout.txt` ),
         "latin1"
       );
 
-      const parseLayout = layout.toString().split("\r\n").slice(1, -1);
+      const parsedLayout = layout
+        .toString()
+        .toLowerCase()
+        .split("\r\n")
+        .slice(1, -1);
+      
+      const parsedData = data
+        .toString()
+        .split("\r\n")
+        .slice(0, -1);
 
-      const parseData = data.toString().split("\r\n").slice(0, -1);
-
-      const resultJson = parseData.map((line, i) => {
-        let obj = {};
-
-        parseLayout.forEach((layout) => {
-          const [coluna, tamanho, inicio, fim, tipo] = layout.split(",");
-
-          obj = {
-            ...obj,
-            [coluna]: line.substr(inicio - 1, tamanho).trim(),
-          };
-        });
-
-        return obj;
-      });
-
-      fs.writeFile(
-        `./generate/${dataset}.json`,
-        JSON.stringify(resultJson),
-        (err) => {
-          if (err) console.error(err);
-          else console.log("Data written to file successfully.");
-        }
+      const resultJson = parsedData.map((line) =>
+        parsedLayout.reduce((obj, layout) => {
+          const [column, size, start, end, type] = layout.split(",");
+          const value = line.substring(start - 1, end).trim();
+          return { ...obj, [column]: value };
+        }, {})
       );
+      
+      await writeFile(
+        path.join(__dirname, "generated", `${dataset}.json`),
+        JSON.stringify(resultJson)
+      );
+
+      console.log(`Data for ${dataset} written to file successfully.`);
     }
-
-} catch (e) {
-  console.log("Error:", e.stack);
+  } catch (err) {
+    console.error("Error:", err.stack);
+  }
 }
+
+convertFiles();
